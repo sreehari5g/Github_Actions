7f4f2a6f9152f2fc023201b59e90e1ddb0f4db46
sreehari5g <sreeharipe305@gmail.com>
2025-02-25 13:22:11 +0530

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `user_details` (
 --
 
 INSERT INTO `user_details` (`user_id`, `username`, `first_name`, `last_name`, `gender`, `password`, `status`) VALUES
-(1, 'rogers60', 'david', 'john', 'Female', 'e6a33eee180b07e563d74fee8c2c66b8', 1),
+(1, 'rogers60', 'Raw', 'john', 'Female', 'e6a33eee180b07e563d74fee8c2c66b8', 1),
 (2, 'mike28', 'rogers', 'paul', 'Male', '2e7dc6b8a1598f4f75c3eaa47958ee2f', 1),
 (3, 'rivera92', 'david', 'john', 'Male', '1c3a8e03f448d211904161a6f5849b68', 1),
 (4, 'ross95', 'maria', 'sanders', 'Male', '62f0a68a4179c5cdd997189760cbcf18', 1),
