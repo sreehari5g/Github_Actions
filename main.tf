5e0f3871f5c4984e82b851a2806a83fd3a3a9242
sreehari5g <sreeharipe305@gmail.com>
2025-02-25 13:31:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 
 provider "azurerm" {
   features {}
-client_id       = "ce3db960-a792-45e7-9c97-9ffc9319b557"
+  client_id       = "ce3db960-a792-45e7-9c97-9ffc9319b557"
   subscription_id = "fc58eee8-2008-4585-9aac-da676d6ba983"
   tenant_id       = "7326fb08-3065-4f2f-a21f-3a64e07a4368"
   # Configuration options
